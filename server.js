013ba2ab30d86209bb2d309ba6bfff3c76f2ac3e
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-04-09 17:10:20 -0400

    server.js mongodb connx updates

--- server.js
+++ server.js
@@ -16,9 +16,16 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, function(err) {
+  if (err)
+  console.log(err);
+});
+mongoose.set('useCreateIndex', true);
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log("API Server now listening on PORT ${PORT}!");
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
