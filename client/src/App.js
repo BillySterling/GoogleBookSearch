81df4af0c5e95326d938d30306550d73babef75b
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-04-11 16:16:09 -0400

    continued code development

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-// import logo from './logo.svg';
-// import './App.css';
-
 class App extends Component {
   render() {
     return (
